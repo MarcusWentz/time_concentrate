40d7aef8fc09fd573c3ec453f7519644d9c603be
Shapley <111250982+0xshapley@users.noreply.github.com>
2023-09-23 14:16:31 -0700

    Create standard_deviation_calc.js
    Axiom standard deviation calculation

---
+++ axiom/standard_deviation_calc.js
@@ -0,0 +1,6 @@
+// incoming array are the past n days (7200 blocks) 
+function getStandardDeviation (array) {
+  const n = array.length
+  const mean = array.reduce((a, b) => a + b) / n
+  return Math.sqrt(array.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n)
+}
