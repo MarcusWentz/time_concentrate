3f453bb41bbb44daf8e882ed4e4be63dad446ed1
MarcusWentz <markzcollege404@gmail.com>
2023-09-23 20:31:17 -0400

    Refactor library progress

--- src/implementation/TradingDaysImplementation.sol
+++ src/implementation/TradingDaysImplementation.sol
@@ -1,23 +1,22 @@
-// // SPDX-License-Identifier: UNLICENSED
-// pragma solidity ^0.8.19;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
 
-// import { TradingDaysHook, HolidayCalendar, DaylightSavingsCalendar } from "../TradingDaysHook.sol";
+import { TradingDaysHook, HolidayCalendar, DaylightSavingsCalendar } from "../TradingDaysHook.sol";
 
-// import { BaseHook } from "periphery-next/BaseHook.sol";
-// import { IPoolManager } from
-//     "v4-core/interfaces/IPoolManager.sol";
-// import { Hooks } from "v4-core/libraries/Hooks.sol";
+import { BaseHook } from "periphery-next/BaseHook.sol";
+import {IPoolManager} from "@uniswap/v4-core/contracts/interfaces/IPoolManager.sol";
+import { Hooks } from "v4-core/libraries/Hooks.sol";
 
-// contract TradingDaysImplementation is TradingDaysHook {
-//     constructor(
-//         IPoolManager poolManager,
-//         HolidayCalendar calendar,
-//         DaylightSavingsCalendar dst,
-//         TradingDaysHook addressToEtch
-//     ) TradingDaysHook(poolManager, calendar, dst) {
-//         Hooks.validateHookAddress(addressToEtch, getHooksCalls());
-//     }
+contract TradingDaysImplementation is TradingDaysHook {
+    constructor(
+        IPoolManager poolManager,
+        HolidayCalendar calendar,
+        DaylightSavingsCalendar dst,
+        TradingDaysHook addressToEtch
+    ) TradingDaysHook(poolManager, calendar, dst) {
+        Hooks.validateHookAddress(addressToEtch, getHooksCalls());
+    }
 
-//     // make this a no-op in testing
-//     function validateHookAddress(BaseHook _this) internal pure override { }
-// }
+    // make this a no-op in testing
+    function validateHookAddress(BaseHook _this) internal pure override { }
+}
