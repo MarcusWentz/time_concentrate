d3a3b5b16d54d214ad6178b2fe8bb339b1fc294b
MarcusWentz <markzcollege404@gmail.com>
2023-09-23 20:23:30 -0400

    Refactor

--- src/TradingDays.sol
+++ src/TradingDays.sol
@@ -1,133 +1,133 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.15;
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.15;
 
-import { BokkyPooBahsDateTimeLibrary as LibDateTime } from
-    "BokkyPooBahsDateTimeLibrary/contracts/BokkyPooBahsDateTimeLibrary.sol";
+// import { BokkyPooBahsDateTimeLibrary as LibDateTime } from
+//     "BokkyPooBahsDateTimeLibrary/contracts/BokkyPooBahsDateTimeLibrary.sol";
 
-import { HolidayCalendar } from "./calendars/HolidayCalendar.sol";
-import { HolidaysLibrary, Holiday } from "./calendars/HolidaysLibrary.sol";
-import { DaylightSavingsCalendar } from
-    "./calendars/DaylightSavingsCalendar.sol";
-import { DaylightSavingsLibrary } from "./calendars/DaylightSavingsLibrary.sol";
+// import { HolidayCalendar } from "./calendars/HolidayCalendar.sol";
+// import { HolidaysLibrary, Holiday } from "./calendars/HolidaysLibrary.sol";
+// import { DaylightSavingsCalendar } from
+//     "./calendars/DaylightSavingsCalendar.sol";
+// import { DaylightSavingsLibrary } from "./calendars/DaylightSavingsLibrary.sol";
 
-/// @title TradingDays
-/// @author horsefacts <horsefacts@terminally.online>
-/// @notice An abstract contract that checks if financial markets are open in
-///         New York. Go Mets, go Jets, go Knicks, go Rangers, greatest city in
-///         the world, baby! If you insist, you can use this without the hook.
-abstract contract TradingDays {
-    using HolidaysLibrary for HolidayCalendar;
-    using DaylightSavingsLibrary for DaylightSavingsCalendar;
-    using LibDateTime for uint256;
+// /// @title TradingDays
+// /// @author horsefacts <horsefacts@terminally.online>
+// /// @notice An abstract contract that checks if financial markets are open in
+// ///         New York. Go Mets, go Jets, go Knicks, go Rangers, greatest city in
+// ///         the world, baby! If you insist, you can use this without the hook.
+// abstract contract TradingDays {
+//     using HolidaysLibrary for HolidayCalendar;
+//     using DaylightSavingsLibrary for DaylightSavingsCalendar;
+//     using LibDateTime for uint256;
 
-    /// @notice Data contract encoding NYSE holidays through 2123.
-    HolidayCalendar public immutable holidays;
+//     /// @notice Data contract encoding NYSE holidays through 2123.
+//     HolidayCalendar public immutable holidays;
 
-    /// @notice Data contract encoding DST start/end timestamps through 2123.
-    DaylightSavingsCalendar public immutable dst;
+//     /// @notice Data contract encoding DST start/end timestamps through 2123.
+//     DaylightSavingsCalendar public immutable dst;
 
-    enum State {
-        HOLIDAY,
-        WEEKEND,
-        AFTER_HOURS,
-        OPEN
-    }
+//     enum State {
+//         HOLIDAY,
+//         WEEKEND,
+//         AFTER_HOURS,
+//         OPEN
+//     }
 
-    /// @notice No trading today, markets are closed for a holiday.
-    error ClosedForHoliday(Holiday holiday);
+//     /// @notice No trading today, markets are closed for a holiday.
+//     error ClosedForHoliday(Holiday holiday);
 
-    /// @notice It's the weekend. Log off and touch grass.
-    error ClosedForWeekend();
+//     /// @notice It's the weekend. Log off and touch grass.
+//     error ClosedForWeekend();
 
-    /// @notice Sorry, everyone in New York already went home.
-    error AfterHours();
+//     /// @notice Sorry, everyone in New York already went home.
+//     error AfterHours();
 
-    constructor(HolidayCalendar _holidays, DaylightSavingsCalendar _dst) {
-        holidays = _holidays;
-        dst = _dst;
-    }
+//     constructor(HolidayCalendar _holidays, DaylightSavingsCalendar _dst) {
+//         holidays = _holidays;
+//         dst = _dst;
+//     }
 
-    /// @notice Return true during core and bot trading hours. 
-    function isCoreTradingHours() public view virtual returns (bool) {
-        uint256 hour = time().getHour();
-        // Core NYSE trading hours 9:30 AM to 4:00 PM ET.
-        if (hour >= 9 && hour < 16) {
-            if (hour == 9) {
-                return time().getMinute() >= 30;
-            }
-            return true;
-        }
-        // Bot overnight trading hours 7:55 PM to 9:00 PM ET. Based Uniswap fees heatmap from Python scripts for this project.
-        if (hour >= 19 && hour < 21) {
-            if (hour == 19) {
-                return time().getMinute() >= 55;
-            }
-            return true;
-        }
-        return false;
-    }
+//     /// @notice Return true during core and bot trading hours. 
+//     function isCoreTradingHours() public view virtual returns (bool) {
+//         uint256 hour = time().getHour();
+//         // Core NYSE trading hours 9:30 AM to 4:00 PM ET.
+//         if (hour >= 9 && hour < 16) {
+//             if (hour == 9) {
+//                 return time().getMinute() >= 30;
+//             }
+//             return true;
+//         }
+//         // Bot overnight trading hours 7:55 PM to 9:00 PM ET. Based Uniswap fees heatmap from Python scripts for this project.
+//         if (hour >= 19 && hour < 21) {
+//             if (hour == 19) {
+//                 return time().getMinute() >= 55;
+//             }
+//             return true;
+//         }
+//         return false;
+//     }
 
-    /// @notice Return true Mon-Fri, if it's not a holiday.
-    function isTradingDay() public view virtual returns (bool) {
-        return time().isWeekDay() && !isHoliday();
-    }
+//     /// @notice Return true Mon-Fri, if it's not a holiday.
+//     function isTradingDay() public view virtual returns (bool) {
+//         return time().isWeekDay() && !isHoliday();
+//     }
 
-    /// @notice Return true if day is a NYSE holiday.
-    function isHoliday() public view virtual returns (bool) {
-        (uint256 year, uint256 month, uint256 day) = time().timestampToDate();
-        return holidays.isHoliday(year, month, day);
-    }
+//     /// @notice Return true if day is a NYSE holiday.
+//     function isHoliday() public view virtual returns (bool) {
+//         (uint256 year, uint256 month, uint256 day) = time().timestampToDate();
+//         return holidays.isHoliday(year, month, day);
+//     }
 
-    /// @notice Get the current holiday from the holiday calendar. Enum values
-    ///         indexed from zero are:
-    ///
-    ///         - NOT_A_HOLIDAY (special value if today is not a holiday)
-    ///         - NEW_YEARS_DAY
-    ///         - MARTIN_LUTHER_KING_JR_DAY
-    ///         - WASHINGTONS_BIRTHDAY
-    ///         - GOOD_FRIDAY
-    ///         - MEMORIAL_DAY
-    ///         - JUNETEENTH_NATIONAL_INDEPENDENCE_DAY
-    ///         - INDEPENDENCE_DAY
-    ///         - LABOR_DAY
-    ///         - THANKSGIVING_DAY
-    ///         - CHRISTMAS_DAY
-    ///         - NEW_YEARS_DAY_OBSERVED
-    ///          (special value if New Year's Day falls on a Saturday)
-    ///
-    function getHoliday() public view virtual returns (Holiday) {
-        (uint256 year, uint256 month, uint256 day) = time().timestampToDate();
-        return holidays.getHoliday(year, month, day);
-    }
+//     /// @notice Get the current holiday from the holiday calendar. Enum values
+//     ///         indexed from zero are:
+//     ///
+//     ///         - NOT_A_HOLIDAY (special value if today is not a holiday)
+//     ///         - NEW_YEARS_DAY
+//     ///         - MARTIN_LUTHER_KING_JR_DAY
+//     ///         - WASHINGTONS_BIRTHDAY
+//     ///         - GOOD_FRIDAY
+//     ///         - MEMORIAL_DAY
+//     ///         - JUNETEENTH_NATIONAL_INDEPENDENCE_DAY
+//     ///         - INDEPENDENCE_DAY
+//     ///         - LABOR_DAY
+//     ///         - THANKSGIVING_DAY
+//     ///         - CHRISTMAS_DAY
+//     ///         - NEW_YEARS_DAY_OBSERVED
+//     ///          (special value if New Year's Day falls on a Saturday)
+//     ///
+//     function getHoliday() public view virtual returns (Holiday) {
+//         (uint256 year, uint256 month, uint256 day) = time().timestampToDate();
+//         return holidays.getHoliday(year, month, day);
+//     }
 
-    /// @notice Return true if it's Daylight Savings Time in New York.
-    function isDST() public view virtual returns (bool) {
-        // The DST calendar stores exact timestamps for start/end of DST in
-        // New York. This and time() should be the only calculations that
-        // use block.timestamp directly. Everything else should use time(),
-        // which adjusts all datetime calculations to US Eastern Time.
-        uint256 year = block.timestamp.getYear();
-        (uint256 start, uint256 end) = dst.getTimestamps(year);
-        return block.timestamp >= start && block.timestamp < end;
-    }
+//     /// @notice Return true if it's Daylight Savings Time in New York.
+//     function isDST() public view virtual returns (bool) {
+//         // The DST calendar stores exact timestamps for start/end of DST in
+//         // New York. This and time() should be the only calculations that
+//         // use block.timestamp directly. Everything else should use time(),
+//         // which adjusts all datetime calculations to US Eastern Time.
+//         uint256 year = block.timestamp.getYear();
+//         (uint256 start, uint256 end) = dst.getTimestamps(year);
+//         return block.timestamp >= start && block.timestamp < end;
+//     }
 
-    /// @notice Adjust block timestamp so that UTC date calculations are
-    ///         localized to US Eastern Time. Subtracts 4 or 5 hours depending
-    ///         on whether it's DST.
-    function time() public view virtual returns (uint256) {
-        return block.timestamp - (isDST() ? 4 hours : 5 hours);
-    }
+//     /// @notice Adjust block timestamp so that UTC date calculations are
+//     ///         localized to US Eastern Time. Subtracts 4 or 5 hours depending
+//     ///         on whether it's DST.
+//     function time() public view virtual returns (uint256) {
+//         return block.timestamp - (isDST() ? 4 hours : 5 hours);
+//     }
 
-    /// @notice Return true if the market is open at current block.timestamp.
-    function marketIsOpen() public view virtual returns (bool) {
-        return state() == State.OPEN;
-    }
+//     /// @notice Return true if the market is open at current block.timestamp.
+//     function marketIsOpen() public view virtual returns (bool) {
+//         return state() == State.OPEN;
+//     }
 
-    /// @notice Return market state at current block.timestamp.
-    function state() public view virtual returns (State) {
-        if (isHoliday()) return State.HOLIDAY;
-        if (time().isWeekEnd()) return State.WEEKEND;
-        if (!isCoreTradingHours()) return State.AFTER_HOURS;
-        return State.OPEN;
-    }
-}
+//     /// @notice Return market state at current block.timestamp.
+//     function state() public view virtual returns (State) {
+//         if (isHoliday()) return State.HOLIDAY;
+//         if (time().isWeekEnd()) return State.WEEKEND;
+//         if (!isCoreTradingHours()) return State.AFTER_HOURS;
+//         return State.OPEN;
+//     }
+// }
--- src/calendars/HolidaysLibrary.sol
+++ src/calendars/HolidaysLibrary.sol
@@ -1,167 +1,167 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.15;
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.15;
 
-import { HolidayCalendar } from "./HolidayCalendar.sol";
+// import { HolidayCalendar } from "./HolidayCalendar.sol";
 
-enum Holiday {
-    NOT_A_HOLIDAY,
-    NEW_YEARS_DAY,
-    MARTIN_LUTHER_KING_JR_DAY,
-    WASHINGTONS_BIRTHDAY,
-    GOOD_FRIDAY,
-    MEMORIAL_DAY,
-    JUNETEENTH_NATIONAL_INDEPENDENCE_DAY,
-    INDEPENDENCE_DAY,
-    LABOR_DAY,
-    THANKSGIVING_DAY,
-    CHRISTMAS_DAY,
-    NEW_YEARS_DAY_OBSERVED
-}
+// enum Holiday {
+//     NOT_A_HOLIDAY,
+//     NEW_YEARS_DAY,
+//     MARTIN_LUTHER_KING_JR_DAY,
+//     WASHINGTONS_BIRTHDAY,
+//     GOOD_FRIDAY,
+//     MEMORIAL_DAY,
+//     JUNETEENTH_NATIONAL_INDEPENDENCE_DAY,
+//     INDEPENDENCE_DAY,
+//     LABOR_DAY,
+//     THANKSGIVING_DAY,
+//     CHRISTMAS_DAY,
+//     NEW_YEARS_DAY_OBSERVED
+// }
 
-library HolidaysLibrary {
-    /// @notice LibHoliday supports 100 years of holidays from 2023 to 2123.
-    ///         Years outside this range revert with this error.
-    error YearNotFound();
+// library HolidaysLibrary {
+//     /// @notice LibHoliday supports 100 years of holidays from 2023 to 2123.
+//     ///         Years outside this range revert with this error.
+//     error YearNotFound();
 
-    /// Revert if a function expecting a holiday is called with NOT_A_HOLIDAY.
-    error NotAHoliday();
+//     /// Revert if a function expecting a holiday is called with NOT_A_HOLIDAY.
+//     error NotAHoliday();
 
-    /// @dev 32 bytes, one EVM word.
-    uint256 private constant ONE_WORD = 0x20;
+//     /// @dev 32 bytes, one EVM word.
+//     uint256 private constant ONE_WORD = 0x20;
 
-    /// @dev Size of the leading STOP opcode byte in the data contract.
-    uint256 private constant STOP_BYTE = 0x1;
+//     /// @dev Size of the leading STOP opcode byte in the data contract.
+//     uint256 private constant STOP_BYTE = 0x1;
 
-    /// @dev One year of holidays, encoded as 13 bytes of contract code.
-    uint256 private constant ONE_YEAR = 0xd;
+//     /// @dev One year of holidays, encoded as 13 bytes of contract code.
+//     uint256 private constant ONE_YEAR = 0xd;
 
-    /// @dev Offset copied bytes by 19 bytes, so the 13 byte holiday schedule
-    //       is laid out at the end of a 32-byte word in scratch space.
-    uint256 private constant COPY_OFFSET = 0x13;
+//     /// @dev Offset copied bytes by 19 bytes, so the 13 byte holiday schedule
+//     //       is laid out at the end of a 32-byte word in scratch space.
+//     uint256 private constant COPY_OFFSET = 0x13;
 
-    /// @dev Alias for scratch space memory address.
-    uint256 private constant SCRATCH_SPACE = 0x0;
+//     /// @dev Alias for scratch space memory address.
+//     uint256 private constant SCRATCH_SPACE = 0x0;
 
-    /// @dev Width of one encoded holiday in bits.
-    uint256 private constant HOLIDAY_BIT_WIDTH = 9;
+//     /// @dev Width of one encoded holiday in bits.
+//     uint256 private constant HOLIDAY_BIT_WIDTH = 9;
 
-    /// @dev Width of one encoded month in bits.
-    uint256 private constant MONTH_BIT_WIDTH = 5;
+//     /// @dev Width of one encoded month in bits.
+//     uint256 private constant MONTH_BIT_WIDTH = 5;
 
-    /// @dev Mask to extract month from an encoded holiday.
-    ///      0x1e0 == 0b111100000
-    uint256 private constant MONTH_BIT_MASK = 0x1e0;
+//     /// @dev Mask to extract month from an encoded holiday.
+//     ///      0x1e0 == 0b111100000
+//     uint256 private constant MONTH_BIT_MASK = 0x1e0;
 
-    /// @dev Mask to extract day from an encoded holiday.
-    ///      0x1f == 0b000011111
-    uint256 private constant DAY_BIT_MASK = 0x1f;
+//     /// @dev Mask to extract day from an encoded holiday.
+//     ///      0x1f == 0b000011111
+//     uint256 private constant DAY_BIT_MASK = 0x1f;
 
-    // There are up to 11 trading holidays in a calendar year, depending on
-    // when New Year's Day is observed. If New Year's Day falls on Sat, it
-    // may be observed on the Friday of the previous year. So we need to
-    // store up to 11 dates per year.
+//     // There are up to 11 trading holidays in a calendar year, depending on
+//     // when New Year's Day is observed. If New Year's Day falls on Sat, it
+//     // may be observed on the Friday of the previous year. So we need to
+//     // store up to 11 dates per year.
 
-    // We store the month and day of each holiday in 9 bits: 4 for the month
-    // and 5 for the day. That makes 99 bits for a full year of holidays,
-    // which fits in 13 bytes. We store each year's calendar as 13 bytes of
-    // contract code, starting from 2023 at offset 1. (The first byte of the
-    // data contract is the STOP opcode.)
+//     // We store the month and day of each holiday in 9 bits: 4 for the month
+//     // and 5 for the day. That makes 99 bits for a full year of holidays,
+//     // which fits in 13 bytes. We store each year's calendar as 13 bytes of
+//     // contract code, starting from 2023 at offset 1. (The first byte of the
+//     // data contract is the STOP opcode.)
 
-    /// @notice Get the month and day of the given holiday in the given year
-    function getDate(HolidayCalendar calendar, Holiday holiday, uint256 year)
-        internal
-        view
-        returns (uint256 month, uint256 day)
-    {
-        if (year < 2023 || year > 2123) revert YearNotFound();
-        if (holiday == Holiday.NOT_A_HOLIDAY) revert NotAHoliday();
+//     /// @notice Get the month and day of the given holiday in the given year
+//     function getDate(HolidayCalendar calendar, Holiday holiday, uint256 year)
+//         internal
+//         view
+//         returns (uint256 month, uint256 day)
+//     {
+//         if (year < 2023 || year > 2123) revert YearNotFound();
+//         if (holiday == Holiday.NOT_A_HOLIDAY) revert NotAHoliday();
 
-        assembly ("memory-safe") {
-            extcodecopy(
-                calendar,
-                COPY_OFFSET,
-                add(STOP_BYTE, mul(sub(year, 2023), ONE_YEAR)),
-                ONE_YEAR
-            )
-            let schedule := mload(SCRATCH_SPACE)
-            let date :=
-                shr(mul(sub(10, sub(holiday, 1)), HOLIDAY_BIT_WIDTH), schedule)
-            month := shr(MONTH_BIT_WIDTH, and(date, MONTH_BIT_MASK))
-            day := and(date, DAY_BIT_MASK)
-        }
-    }
+//         assembly ("memory-safe") {
+//             extcodecopy(
+//                 calendar,
+//                 COPY_OFFSET,
+//                 add(STOP_BYTE, mul(sub(year, 2023), ONE_YEAR)),
+//                 ONE_YEAR
+//             )
+//             let schedule := mload(SCRATCH_SPACE)
+//             let date :=
+//                 shr(mul(sub(10, sub(holiday, 1)), HOLIDAY_BIT_WIDTH), schedule)
+//             month := shr(MONTH_BIT_WIDTH, and(date, MONTH_BIT_MASK))
+//             day := and(date, DAY_BIT_MASK)
+//         }
+//     }
 
-    /// @notice Get all holidays in the given year, as month/day arrays.
-    ///         index in the array is the same as the Holiday enum value.
-    function getAllDates(HolidayCalendar calendar, uint256 year)
-        internal
-        view
-        returns (uint256[11] memory months, uint256[11] memory dates)
-    {
-        if (year < 2023 || year > 2123) revert YearNotFound();
+//     /// @notice Get all holidays in the given year, as month/day arrays.
+//     ///         index in the array is the same as the Holiday enum value.
+//     function getAllDates(HolidayCalendar calendar, uint256 year)
+//         internal
+//         view
+//         returns (uint256[11] memory months, uint256[11] memory dates)
+//     {
+//         if (year < 2023 || year > 2123) revert YearNotFound();
 
-        assembly ("memory-safe") {
-            extcodecopy(
-                calendar,
-                COPY_OFFSET,
-                add(STOP_BYTE, mul(sub(year, 2023), ONE_YEAR)),
-                ONE_YEAR
-            )
-            let schedule := mload(SCRATCH_SPACE)
-            let monthPtr := add(months, mul(10, ONE_WORD))
-            let dayPtr := add(dates, mul(10, ONE_WORD))
-            for { let i := 11 } gt(i, 0) { i := sub(i, 1) } {
-                let date := shr(mul(sub(11, i), HOLIDAY_BIT_WIDTH), schedule)
-                mstore(
-                    monthPtr, shr(MONTH_BIT_WIDTH, and(date, MONTH_BIT_MASK))
-                )
-                mstore(dayPtr, and(date, DAY_BIT_MASK))
-                monthPtr := sub(monthPtr, ONE_WORD)
-                dayPtr := sub(dayPtr, ONE_WORD)
-            }
-        }
-    }
+//         assembly ("memory-safe") {
+//             extcodecopy(
+//                 calendar,
+//                 COPY_OFFSET,
+//                 add(STOP_BYTE, mul(sub(year, 2023), ONE_YEAR)),
+//                 ONE_YEAR
+//             )
+//             let schedule := mload(SCRATCH_SPACE)
+//             let monthPtr := add(months, mul(10, ONE_WORD))
+//             let dayPtr := add(dates, mul(10, ONE_WORD))
+//             for { let i := 11 } gt(i, 0) { i := sub(i, 1) } {
+//                 let date := shr(mul(sub(11, i), HOLIDAY_BIT_WIDTH), schedule)
+//                 mstore(
+//                     monthPtr, shr(MONTH_BIT_WIDTH, and(date, MONTH_BIT_MASK))
+//                 )
+//                 mstore(dayPtr, and(date, DAY_BIT_MASK))
+//                 monthPtr := sub(monthPtr, ONE_WORD)
+//                 dayPtr := sub(dayPtr, ONE_WORD)
+//             }
+//         }
+//     }
 
-    /// @notice Get the Holiday for a given date. Returns special value
-    ///         Holiday.NOT_A_HOLIDAY if the date is not a holiday.
-    function getHoliday(
-        HolidayCalendar calendar,
-        uint256 year,
-        uint256 month,
-        uint256 day
-    ) internal view returns (Holiday h) {
-        if (year < 2023 || year > 2123) revert YearNotFound();
+//     /// @notice Get the Holiday for a given date. Returns special value
+//     ///         Holiday.NOT_A_HOLIDAY if the date is not a holiday.
+//     function getHoliday(
+//         HolidayCalendar calendar,
+//         uint256 year,
+//         uint256 month,
+//         uint256 day
+//     ) internal view returns (Holiday h) {
+//         if (year < 2023 || year > 2123) revert YearNotFound();
 
-        assembly ("memory-safe") {
-            extcodecopy(
-                calendar,
-                COPY_OFFSET,
-                add(STOP_BYTE, mul(sub(year, 2023), ONE_YEAR)),
-                ONE_YEAR
-            )
-            let schedule := mload(SCRATCH_SPACE)
-            for { let i := 11 } gt(i, 0) { i := sub(i, 1) } {
-                let date := shr(mul(sub(11, i), HOLIDAY_BIT_WIDTH), schedule)
-                let _isHoliday :=
-                    and(
-                        eq(month, shr(MONTH_BIT_WIDTH, and(date, MONTH_BIT_MASK))),
-                        eq(day, and(date, DAY_BIT_MASK))
-                    )
-                if _isHoliday {
-                    h := i
-                    break
-                }
-            }
-        }
-    }
+//         assembly ("memory-safe") {
+//             extcodecopy(
+//                 calendar,
+//                 COPY_OFFSET,
+//                 add(STOP_BYTE, mul(sub(year, 2023), ONE_YEAR)),
+//                 ONE_YEAR
+//             )
+//             let schedule := mload(SCRATCH_SPACE)
+//             for { let i := 11 } gt(i, 0) { i := sub(i, 1) } {
+//                 let date := shr(mul(sub(11, i), HOLIDAY_BIT_WIDTH), schedule)
+//                 let _isHoliday :=
+//                     and(
+//                         eq(month, shr(MONTH_BIT_WIDTH, and(date, MONTH_BIT_MASK))),
+//                         eq(day, and(date, DAY_BIT_MASK))
+//                     )
+//                 if _isHoliday {
+//                     h := i
+//                     break
+//                 }
+//             }
+//         }
+//     }
 
-    /// @notice Return true if the given year/month/day is a holiday.
-    function isHoliday(
-        HolidayCalendar calendar,
-        uint256 year,
-        uint256 month,
-        uint256 day
-    ) internal view returns (bool _isHoliday) {
-        return getHoliday(calendar, year, month, day) != Holiday.NOT_A_HOLIDAY;
-    }
-}
+//     /// @notice Return true if the given year/month/day is a holiday.
+//     function isHoliday(
+//         HolidayCalendar calendar,
+//         uint256 year,
+//         uint256 month,
+//         uint256 day
+//     ) internal view returns (bool _isHoliday) {
+//         return getHoliday(calendar, year, month, day) != Holiday.NOT_A_HOLIDAY;
+//     }
+// }
